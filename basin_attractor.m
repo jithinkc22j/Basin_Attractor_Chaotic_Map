cff4e459599a99d6826bcc45ad118335976bbc1f
JITHIN K C <jithinkc22@gmail.com>
2020-05-20 18:42:12 +0530

    Add files via upload

---
+++ basin_attractor.m
@@ -0,0 +1,14 @@
+function [out]=basin_attractor(x,y,a,siz)
+n=siz*siz;
+
+for i=1:n
+% If you need complete code contct me
+
+end
+
+x1= reshape(x1,[siz,siz]);
+y1= reshape(y1,[siz,siz]);
+plot(y1,x1,'DisplayName','x1')
+out=x1;
+end
+
